c92324ae5a380f2286437620b4498de4f1845973
theahsanusman <ahsanusmanabduljabbar@gmail.com>
2021-07-27 20:56:43 +0500

    learned sha256 basics and create hash for each block

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256')
+
 // Contructor Function
 function BlockChain() {
   this.chain = []
@@ -9,20 +11,43 @@ function BlockChain() {
 // NEW BLOCK HASH IS GENERATED BASED UPON PREV.. BLOCK HASHES
 // SHA256 algo with be used for HASH
 
-BlockChain.prototype.createNewBlock = function (nounce, prevBlockHash, hash) {
+BlockChain.prototype.createNewBlock = function (nounce, prevBlockHash) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date(Date.now()),
     transactions: this.pendingTransactions,
     nounce,
     prevBlockHash,
-    hash,
   }
-
+  const hash = this.blockHash(prevBlockHash, newBlock)
+  newBlock.hash = hash
   this.chain.push(newBlock)
   this.pendingTransactions = []
 
   return newBlock
+}
+
+BlockChain.prototype.lastBlock = function () {
+  return this.chain[this.chain.length - 1]
+}
+
+BlockChain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient,
+) {
+  const newTransaction = {
+    amount,
+    sender,
+    recipient,
+  }
+
+  this.pendingTransactions.push(newTransaction)
+}
+
+BlockChain.prototype.blockHash = function (prevBlockHash, currentBlockData) {
+  const dataString = `${prevBlockHash}${currentBlockData}`
+  return sha256(dataString)
 }
 
 module.exports = BlockChain
--- 1/tests.js
+++ 1/tests.js
@@ -1,11 +1,19 @@
 const BlockChain = require('./index')
 
 const Bitcoin = new BlockChain()
+const chain = Bitcoin.chain;
+
 
 Bitcoin.createNewBlock(
-    12423,
-    '3243242',
-    '3532342'
-);
+  12423,
+  '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B',
+)
+Bitcoin.createNewBlock(4644, chain[chain.length - 1].hash)
 
-console.log(Bitcoin.chain)
+Bitcoin.createNewTransaction(100, 'senvnio', 'recipeint.......')
+Bitcoin.createNewTransaction(200, 'senvnio', 'recipeint.......')
+Bitcoin.createNewTransaction(50, 'senvnio', 'recipeint.......')
+
+Bitcoin.createNewBlock(999, chain[chain.length - 1].hash)
+
+console.log(chain)
